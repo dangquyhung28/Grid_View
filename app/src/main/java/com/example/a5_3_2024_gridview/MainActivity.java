2a6208f5cefec12bb2a33fbf8567ca40741b68a0
DQHung <quyhung0121@gmail.com>
2024-05-27 21:15:19 +0700

    bai tap gridview

---
+++ app/src/main/java/com/example/a5_3_2024_gridview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a5_3_2024_gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+   
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initwiget();
+    }
+
+    private void initwiget() {
+    }
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "5_3_2024_GridView"
+include(":app")
+ 
